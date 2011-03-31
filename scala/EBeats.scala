f0825cef00cb1ae75391f2b240c5d91baaf63727
Paul Lambert <paul@paulitex.com>
2011-03-31 14:03:02 -0700

    Adding scala implementation

---
+++ scala/EBeats.scala
@@ -0,0 +1,11 @@
+import java.util.{Calendar, TimeZone}
+import Calendar._
+
+object EBeats extends Application {
+    val now = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    val beats = List(   now.get(HOUR_OF_DAY) * (1000.0 / 24.0), 
+                        now.get(MINUTE) * (1000.0 / (24.0 * 60.0)), 
+                        now.get(SECOND) * (1000.0 / (24.0 * 60.0 * 60.0))).sum
+                    
+    println("@%.2f".format(beats))
+}
